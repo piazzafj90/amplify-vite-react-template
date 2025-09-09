94bc00d01c7c006b2a693877f06b50b29bb96eaa
Frank Piazza <fr@nkpiazza.com>
2025-09-09 16:10:18 +0200

    first deploy with new theme

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import MarketingPage from '../src/pages/MarketingPage';
+
+export default function App() {
+  return (
+    <Container maxWidth="med">
+      <MarketingPage />
+    </Container>
+  );
+}
